c6a8112cc043e5eff6ab2496f5ef8538086696fb
Pethum Chamara Vanniarachchi <pathumc4@gmail.com>
2025-05-13 15:39:19 +0530

    Slider images and overlay done

--- src/components/Landing/SlideShow.jsx
+++ src/components/Landing/SlideShow.jsx
@@ -1,49 +1,122 @@
-import React from 'react'
-import spare from '../../assets/spare.jpg'
-import second from '../../assets/second.webp'
-import third from '../../assets/third.jpg'
+import React from "react";
+import spare from "../../assets/slider/uslider_01.png";
+import second from "../../assets/slider/uslider_02.png";
+import third from "../../assets/slider/uslider_03.png";
 
 export default function SlideShow() {
   return (
     <div>
-    <div id="carouselExampleDark" class="carousel carousel-dark slide">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active" data-bs-interval="10000">
-      <img src={spare} class="d-block w-100" alt="..."/>
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
+      <div
+        id="carouselExampleCaptions"
+        className="carousel slide "
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="0"
+            class="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <div className="carousel-overlay"></div> {/* Overlay */}
+            <img src={spare} className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+              <div>
+                <span>Unleash True Power</span>
+              </div>
+
+              <div>
+                <h2>Top Grade Motorcycle Parts</h2>
+              </div>
+
+              <div>
+                <button type="button" className="btn btn-info">
+                  Shop Now
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <div className="carousel-overlay"></div> {/* Overlay */}
+            <img src={second} className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+              <div>
+                <span>Style Meets Function</span>
+              </div>
+
+              <div>
+                <h2>Upgrade Your Ride's Look</h2>
+              </div>
+
+              <div>
+                <button type="button" className="btn btn-info">
+                  Browse Accessories
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <div className="carousel-overlay"></div> {/* Overlay */}
+            <img src={third} className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+              <div>
+                <span>Keep It Running Smooth</span>
+              </div>
+
+              <div>
+                <h2>Essential Tools & Fluids</h2>
+              </div>
+
+              <div>
+                <button type="button" className="btn btn-info">
+                  Shop Essentials
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
       </div>
     </div>
-    <div class="carousel-item" data-bs-interval="2000">
-      <img src={second} class="d-block w-100" alt="..."/>
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src={third} class="d-block w-100" alt="..."/>
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-    </div>
-  )
+  );
 }
