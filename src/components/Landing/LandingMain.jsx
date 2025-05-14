8eac12729d4778ae575d16360ad8865ef54ba52e
ChanduniNethmini <nchanduni21@gmail.com>
2025-05-14 20:32:14 +0530

    add cart and item page

---
+++ src/components/Landing/LandingMain.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SlideShow from './SlideShow';
+import CardList from './CardList'
+
+export default function LandingMain() {
+  return (
+    <div>
+      <SlideShow />
+      <CardList />
+    </div>
+  );
+}
